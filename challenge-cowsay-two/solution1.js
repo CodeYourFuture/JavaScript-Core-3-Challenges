2110d3f3e073291cb9fac3bea40a584f338868ca
Sally McGrath <sally@codeyourfuture.io>
2021-08-04 16:06:25 +0100

    cowsay CLI project with scaffolding
    https://github.com/CodeYourFuture/syllabus/pull/189

---
+++ challenge-cowsay-two/solution1.js
@@ -0,0 +1,30 @@
+// =================
+// Stripped down cowsayer CLI, 
+// no libraries
+// https://nodejs.dev/learn/nodejs-accept-arguments-from-the-command-line
+// =================
+
+// 1. Accept arguments
+
+// how will you accept arguments?
+
+// 2. Make supplies for our speech bubble
+
+let topLine = '_';
+let bottomLine = '-';
+let saying = '';
+
+// 3. Make a cow that takes a string
+
+function cowsay(saying) {
+// how will you make the speech bubble contain the text?
+
+// where will the cow picture go?
+
+// how will you account for the parameter being empty?
+
+}
+
+//4. Pipe argument into cowsay function and return a cow
+
+// how will you log this to the console?
