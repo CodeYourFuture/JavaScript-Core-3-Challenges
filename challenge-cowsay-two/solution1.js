f5d37d2360cef9512fa0c7786bd27107d43a88d4
Haroon Ali <87202358+haroon-ali-dev@users.noreply.github.com>
2022-11-06 16:37:58 +0000

    complete cow say challenge

--- challenge-cowsay-two/solution1.js
+++ challenge-cowsay-two/solution1.js
@@ -9,22 +9,44 @@
 // how will you accept arguments?
 
 // 2. Make supplies for our speech bubble
-
-let topLine = '_';
-let bottomLine = '-';
+let topLine = '_________';
+let bottomLine = '----------';
 let saying = '';
 
 // 3. Make a cow that takes a string
 
 function cowsay(saying) {
-// how will you make the speech bubble contain the text?
+  // how will you make the speech bubble contain the text?
+  saying = `
+      ${topLine}
 
-// where will the cow picture go?
-
-// how will you account for the parameter being empty?
+      ${getSaying(process.argv)}
+      ${bottomLine}`;
+  // where will the cow picture go?
+  makeCow(saying);
+  // how will you account for the parameter being empty?
 
 }
 
 //4. Pipe argument into cowsay function and return a cow
 
 // how will you log this to the console?
+cowsay(saying);
+
+function getSaying(argV) {
+  argV.shift();
+  argV.shift();
+
+  return argV.join(" ");
+}
+
+function makeCow(saying) {
+  console.log(saying);
+  console.log("       /");
+  console.log("      /");
+  console.log("^__^ /");
+  console.log("(oo)'");
+  console.log("(__)        )-~");
+  console.log("   ||----w |");
+  console.log("   ||     ||");
+}
--- challenge-cowsay-two/solution2.js
+++ challenge-cowsay-two/solution2.js
@@ -5,14 +5,57 @@
 // =================
 
 // 1. Make  a command line interface.
+const prompt = require('prompt');
+
+prompt.start();
+
+prompt.get(['saying'], function (err, result) {
+    if (err) {
+        return onErr(err);
+    }
+
+    cow(result.saying);
+});
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
 
 // 2. Make supplies for our speech bubble
+let topLine = '_________';
+let bottomLine = '----------';
+let saying = '';
 
 // 3. Make a cow that takes a string
 
+
 const cow = (saying) => {
-    // how did you make the cow before?
+    saying = `
+      ${topLine}
+
+      ${saying}
+      ${bottomLine}`;
+
+    makeCow(saying);
 }
 
 // 4. Use readline to get a string from the terminal 
 // (with a prompt so it's clearer what we want)
+function getSaying(argV) {
+    argV.shift();
+    argV.shift();
+
+    return argV.join(" ");
+}
+
+function makeCow(saying) {
+    console.log(saying);
+    console.log("       /");
+    console.log("      /");
+    console.log("^__^ /");
+    console.log("(oo)'");
+    console.log("(__)        )-~");
+    console.log("   ||----w |");
+    console.log("   ||     ||");
+}
