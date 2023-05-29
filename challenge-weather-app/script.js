8d73fe9cdf6df84ad8a4984e93fd1524c4ab134d
Olha Danylevska <odanilevskaya2@gmail.com>
2023-05-29 15:56:05 +0100

    fetch weather data and photos

--- challenge-weather-app/script.js
+++ challenge-weather-app/script.js
@@ -1,40 +1,28 @@
 let weatherLink = `http://api.openweathermap.org/data/2.5/weather?q=london&APPID=ae83a519b40c97351fe6959e241b5f97`
-let photoLink = `https://api.unsplash.com/search/photos?query=snow&client_id=QDJfxR32eNpAd3Kr75Crhg5OO1KHsR4OE5PBHpB0R7g`
-let arrayofPhotos 
+let weather
+let photoLink = `https://api.unsplash.com/search/photos?query=${weather}&client_id=QDJfxR32eNpAd3Kr75Crhg5OO1KHsR4OE5PBHpB0R7g`
+let arrayofPhotos
+let photo
 
-let weatherFetch = async () =>{
-fetch(weatherLink)
+
+
+
+function photoFetch() {
+  fetch(photoLink)
     .then((response) => {
-      if (response.ok){
+      if (response.ok) {
         return response.json();
       } else {
-        console.log("Error") 
+        console.log("Error")
       }
     })
     .then((data) => {
-      console.log("typeof", data);
-      return data
-    });   
-}
+      let { results } = data
+      arrayofPhotos = results
+      console.log("arry", arrayofPhotos)
+      displayThePhotosinThumb(arrayofPhotos)
 
-weatherFetch ()
-
-
-function photoFetch (){
-fetch(photoLink)
-    .then((response) => {
-      if (response.ok){
-        return response.json();
-      } else {
-        console.log("Error") 
-      }
-    })
-    .then((data) => {
-        let {results} = data
-        arrayofPhotos = results
-        console.log("arry", arrayofPhotos)
-        displayThePhotos(arrayofPhotos)
-    });   
+    });
 }
 photoFetch()
 
@@ -43,20 +31,63 @@ photoFetch()
 // let photoArea = document.querySelector("#photo")
 
 
-function displayThePhotos(array){
-    
-    for (let elemet of array){
-        let thumbs = document.querySelector("#thumbs")
-        let photo = document.createElement("img")
-        let url = elemet.urls.thumb
-        photo.src = url
-        thumbs.appendChild(photo)
-    }
-  
-    
+function displayThePhotosinThumb(array) {
+  for (let elemet of array) {
+    let thumbs = document.querySelector("#thumbs")
+    photo = document.createElement("img")
+    let url = elemet.urls.full
+
+    photo.src = url
+    thumbs.appendChild(photo)
+  }
+
+
+}
+thumbs.addEventListener("click", displayBigPhoto)
+
+function displayBigPhoto(event) {
+  if (event.target.nodeName.toLowerCase() == "img") {
+    let bigPhotoContainer = document.querySelector("#photo")
+    bigPhotoContainer.innerHTML = ""
+    let bigPhoto = document.createElement("img")
+    let url = event.target.src
+    bigPhoto.src = url
+    bigPhotoContainer.appendChild(bigPhoto)
+  }
+}
+
+// taking our location and doing fetch
+
+getLocationData()
+function getLocationData() {
+  navigator.geolocation.getCurrentPosition((success) => {
+    console.log("ok", success)
+    let { latitude, longitude } = success.coords
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=ae83a519b40c97351fe6959e241b5f97`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.log("Error")
+        }
+      })
+      .then((data) => {
+        console.log("Data", data);
+        displayCarrentWether(data)
+        weather = data.weather[0].description
+
+      });
+
+  })
+}
+
+function displayCarrentWether(data) {
+  let conditionsP = document.querySelector("#conditions")
+  let { weather } = data
+
+  conditionsP.innerHTML = `Overcast - ${weather[0].description}`
 
 }
 
 
 
-    
