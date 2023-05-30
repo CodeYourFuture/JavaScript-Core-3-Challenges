5df42d836cfb8773df4aff584972e7615eca2723
Olha Danylevska <odanilevskaya2@gmail.com>
2023-05-30 11:03:38 +0100

    finished weather app

--- challenge-weather-app/script.js
+++ challenge-weather-app/script.js
@@ -1,14 +1,14 @@
-let weatherLink = `http://api.openweathermap.org/data/2.5/weather?q=london&APPID=ae83a519b40c97351fe6959e241b5f97`
 let weather
-let photoLink = `https://api.unsplash.com/search/photos?query=${weather}&client_id=QDJfxR32eNpAd3Kr75Crhg5OO1KHsR4OE5PBHpB0R7g`
 let arrayofPhotos
 let photo
+let latitude
+let longitude
 
 
+// fetching and search photos with string from description of weather
 
-
-function photoFetch() {
-  fetch(photoLink)
+function photoFetch(weather) {
+  fetch(`https://api.unsplash.com/search/photos?query=${weather}&client_id=QDJfxR32eNpAd3Kr75Crhg5OO1KHsR4OE5PBHpB0R7g`)
     .then((response) => {
       if (response.ok) {
         return response.json();
@@ -24,19 +24,41 @@ function photoFetch() {
 
     });
 }
-photoFetch()
 
 
+let goButton = document.querySelector(".btn")
+goButton.addEventListener("click", clickTheButtonGo)
 
-// let photoArea = document.querySelector("#photo")
+// after clicking Go button
+
+function clickTheButtonGo(event) {
+  event.preventDefault()
+  let searchBar = document.querySelector("#search-tf").value
+  console.log('searchString', searchBar)
+
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchBar}&limit=5&appid=ae83a519b40c97351fe6959e241b5f97`)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log("Error")
+      }
+    })
+    .then((data) => {
+      console.log("Data Chicago", data[0]);
+      latitude = data[0].lat
+      longitude = data[0].lat
+      getLocationData(latitude, longitude)
+    });
+}
 
 
 function displayThePhotosinThumb(array) {
+  let thumbs = document.querySelector("#thumbs")
+  thumbs.innerHTML = ""
   for (let elemet of array) {
-    let thumbs = document.querySelector("#thumbs")
     photo = document.createElement("img")
     let url = elemet.urls.full
-
     photo.src = url
     thumbs.appendChild(photo)
   }
@@ -58,36 +80,34 @@ function displayBigPhoto(event) {
 
 // taking our location and doing fetch
 
-getLocationData()
-function getLocationData() {
-  navigator.geolocation.getCurrentPosition((success) => {
-    console.log("ok", success)
-    let { latitude, longitude } = success.coords
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=ae83a519b40c97351fe6959e241b5f97`)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          console.log("Error")
-        }
-      })
-      .then((data) => {
-        console.log("Data", data);
-        displayCarrentWether(data)
-        weather = data.weather[0].description
 
-      });
-
-  })
+function getLocationData(latitude, longitude) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=ae83a519b40c97351fe6959e241b5f97`)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log("Error1")
+      }
+    })
+    .then((success) => {
+      displayCarrentWether(success)
+      weather = success.weather[0].description
+      photoFetch(weather)
+    });
 }
 
-function displayCarrentWether(data) {
+function displayCarrentWether(info) {
   let conditionsP = document.querySelector("#conditions")
-  let { weather } = data
+  let { weather } = info
 
   conditionsP.innerHTML = `Overcast - ${weather[0].description}`
-
 }
+
+  // navigator.geolocation.getCurrentPosition((success) => {
+  //   console.log("ok", success)
+  //   let { latitude, longitude } = success.coords
+
 
 
 
