2d855e917749ab96431a07d19ea0f9dd50e16925
nasir ali <a.nasirali@outlook.com>
2023-06-07 14:43:49 +0100

    made the cow says ascii cow

--- challenge-cowsay-two/solution2.js
+++ challenge-cowsay-two/solution2.js
@@ -1,18 +1,43 @@
 // =================
-// Stripped down cowsayer CLI, 
+// Stripped down cowsayer CLI,
 // no libraries or arguments
 // https://nodejs.dev/learn/accept-input-from-the-command-line-in-nodejs
 // =================
 
 // 1. Make  a command line interface.
+const readline = require("readline");
+
+// Create a readline interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 // 2. Make supplies for our speech bubble
+// Function to create the speech bubble around the saying
+function createSpeechBubble(saying) {
+  const speechLength = saying.length + 2;
+  const speechBubble = `${"-".repeat(speechLength)}
+< ${saying} >
+${"-".repeat(speechLength)}
+         \\   ^__^
+          \\  (oo)\\_______
+             (__)\\       )\\/\\
+                 ||----w |
+                 ||     ||
+`;
 
-// 3. Make a cow that takes a string
-
-const cow = (saying) => {
-    // how did you make the cow before?
+  return speechBubble;
 }
 
-// 4. Use readline to get a string from the terminal 
-// (with a prompt so it's clearer what we want)
+// 3. Make a cow that takes a string
+// Prompt the user for the saying
+rl.question("What should the cow say? ", (saying) => {
+  rl.close();
+
+  // Generate the cow with the speech bubble
+  const cow = createSpeechBubble(saying);
+
+  // Print the cow to the console
+  console.log(cow);
+});
