75548c632fce47dd22e8c26177decea671294b77
Bakhat Begum <bakhtbagum@gmail.com>
2024-02-15 22:43:06 +0000

    i added functio for ASCII cow image and talk

--- challenge-cowsay-two/solution1.js
+++ challenge-cowsay-two/solution1.js
@@ -1,5 +1,5 @@
 // =================
-// Stripped down cowsayer CLI, 
+// Stripped down cowSayer CLI, 
 // no libraries
 // https://nodejs.dev/learn/nodejs-accept-arguments-from-the-command-line
 // =================
@@ -7,24 +7,48 @@
 // 1. Accept arguments
 
 // how will you accept arguments?
+const arguments = process.argv.slice(2);     // this line process command argument when running the script name "argument " and "node".
 
 // 2. Make supplies for our speech bubble
 
-let topLine = '_';
-let bottomLine = '-';
-let saying = '';
+let topLine = '_';                            //the topLine variable set up a line at the top.
+let bottomLine = '-';                         //the bottomLine variable set up a line at the  bottom.
+let saying = '';                              // we use an empty string to use it as a default later.
 
 // 3. Make a cow that takes a string
 
-function cowsay(saying) {
+function cowSay(saying) {
 // how will you make the speech bubble contain the text?
+let speechBubble = `${saying}`;
 
 // where will the cow picture go?
+let cowPicture = 
+`
+       /
+      /
+^__^ /
+(oo)'_______
+(__)        )-~
+   ||----w |
+   ||     ||
+`;
 
 // how will you account for the parameter being empty?
+    if(!saying){
+        speechBubble = ' Moo! ' ;
+    }
 
-}
+    //here the line bring entire cow message.
+    return `${topLine.repeat(speechBubble.length)}\n${speechBubble}\n${bottomLine.repeat(speechBubble.length)}${cowPicture}`
 
-//4. Pipe argument into cowsay function and return a cow
+};
+
+//4. Pipe argument into cowSay function and return a cow
 
 // how will you log this to the console?
+console.log(cowSay(arguments.join(' ')));                   // this line mean join arguments line into a single string
+
+
+
+
+
--- challenge-cowsay-two/solution2.js
+++ challenge-cowsay-two/solution2.js
@@ -1,18 +1,50 @@
 // =================
-// Stripped down cowsayer CLI, 
+// Stripped down cowSayer CLI, 
 // no libraries or arguments
 // https://nodejs.dev/learn/accept-input-from-the-command-line-in-nodejs
 // =================
 
 // 1. Make  a command line interface.
-
+const arguments = process.argv.slice(2); 
 // 2. Make supplies for our speech bubble
 
-// 3. Make a cow that takes a string
+let topLine = '_';                           
+let bottomLine = '-';                        
+let saying = '';
 
+// 3. Make a cow that takes a string
 const cow = (saying) => {
+    let speechBubble = ` ${saying} `;
     // how did you make the cow before?
+    let cowPicture = 
+`
+       /
+      /
+^__^ /
+(oo)'_______
+(__)        )-~
+   ||----w |
+   ||     ||
+`;
+
+if(!saying){
+    console.log(" Moo! ");
+}
+return `${topLine}\n${speechBubble}\n${bottomLine.repeat(saying.length)}${cowPicture}`
+
 }
 
 // 4. Use readline to get a string from the terminal 
 // (with a prompt so it's clearer what we want)
+
+const readline = require('readline');               // "readline" module provides an interface for reading data from a Readable stream (such as process.stdin and stdout) one line at a time.
+
+const resultLine = readline.createInterface({
+  input: process.stdin,                      // the create interface for readline input(stdin) & output(stdout) mean. it will read input form terminal (stdin) and write output to terminal ("stdout");
+  output:process.stdout
+});
+
+resultLine.question('The cow say? ', (saying) => {      // this line prompts the user with the question in terminal and pass the argument as a callback function(saying) 
+  resultLine.close();
+});
+console.log(cow("you are cute"));
